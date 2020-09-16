65fd811dd5ec67fb62c89d0a9b750b212c17745a
Razexay <razexay@gmail.com>
2020-09-16 11:14:09 +0300

    Tere

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href=" https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
     <title>WSL 2</title>
 </head>
 
-<body>
-    <table class="w-full">
+<body class="p-12">
+    <table class="w-full table-auto">
+        <thead class="font-bold">
+            <tr>
+                <td class="px-4 py-2">Pealkiri</td>
+                <td class="px-4 py-2">Autor</td>
+                <td class="px-4 py-2">Aasta</td>
+                <td class="px-4 py-2">Keel</td>
+                <td class="px-4 py-2">Lehekülgi</td>
+                <td class="px-4 py-2">Hind</td>
+                <td class="px-4 py-2">Laoseis</td>
+                <td class="px-4 py-2">Tüüp</td>
+            </tr>
+        </thead>
+        <tbody class="text-sm">
+            @foreach($books as $book)
+            <tr>
+                <td class="border px-4 py-2">{{ $book->title }}</td>
+                <td class="border px-4 py-2"></td>
+                <td class="border px-4 py-2">{{ $book->release_date }}</td>
+                <td class="border px-4 py-2">{{ $book->language }}</td>
+                <td class="border px-4 py-2">{{ $book->pages }}</td>
+                <td class="border px-4 py-2">{{ $book->price }}</td>
+                <td class="border px-4 py-2">{{ $book->stock_saldo }}</td>
+                <td class="border px-4 py-2">{{ $book->type }}</td>
+            </tr>
+            @endforeach
     </table>
 </body>
 
